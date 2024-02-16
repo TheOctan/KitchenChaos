7e3dbde6d2bda4411e4251d444069db0ab154786
TheOctan <kanatush.tim@gmail.com>
2024-02-16 18:01:27 +0300

    Collision detecting

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -5,6 +5,9 @@ namespace OctanGames
 {
     public class Player : MonoBehaviour
     {
+        private const float PLAYER_RADIUS = 0.7f;
+        private const float PLAYER_HEIGHT = 2f;
+
         [Header("Parameters")]
         [SerializeField] private float _moveSpeed = 7f;
         [SerializeField] private float _rotateSpeed = 10f;
@@ -21,8 +24,47 @@ namespace OctanGames
             Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
             _moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
-            transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
+            bool canMove = CanMove(_moveDirection);
+
+            if (!canMove)
+            {
+                Vector3 moveDirectionX = new Vector3(_moveDirection.x, 0, 0).normalized;
+                canMove = CanMove(moveDirectionX);
+
+                if (canMove)
+                {
+                    _moveDirection = moveDirectionX;
+                }
+                else
+                {
+                    Vector3 moveDirectionZ = new Vector3(0, 0, _moveDirection.z).normalized;
+                    canMove = CanMove(moveDirectionZ);
+
+                    if (canMove)
+                    {
+                        _moveDirection = moveDirectionZ;
+                    }
+                }
+            }
+
+            if (canMove)
+            {
+                transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
+            }
             transform.forward = Vector3.Slerp(transform.forward, _moveDirection, Time.deltaTime * _rotateSpeed);
+        }
+
+        private bool CanMove(Vector3 moveDirection)
+        {
+            float moveDistance = _moveSpeed * Time.deltaTime;
+            Vector3 position = transform.position;
+
+            bool canMove = !Physics.CapsuleCast(position,
+                position + Vector3.up * PLAYER_HEIGHT,
+                PLAYER_RADIUS,
+                moveDirection,
+                moveDistance);
+            return canMove;
         }
     }
 }
